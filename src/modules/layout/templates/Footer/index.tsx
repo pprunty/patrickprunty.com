78af78fb089b4332de8d05867109b2512d8ee761
pprunty <pprunty@tcd.ie>
2025-01-10 18:36:49 +0000

    updates

--- src/modules/layout/templates/Footer/index.tsx
+++ src/modules/layout/templates/Footer/index.tsx
@@ -2,45 +2,28 @@
 
 import Link from 'next/link';
 import Image from 'next/image';
+import { usePathname } from 'next/navigation';
 import { SocialPlatform } from '@/__samwise/types/SocialPlatform';
 import { urlMapping, SocialIcon } from '@/modules/common/components/SocialIcon';
 import Subscribe from '@/modules/blog/components/Subscribe';
 import { HR } from '@/app/blog/components/hr';
 
 export default function Footer() {
+  const pathname = usePathname();
+  const isBlogPage = pathname.includes('blog');
+
   return (
     <>
-      {/*
-        Dotted line
-        Light mode: dots = #fcfcfc on #1c1c1c background
-        Dark mode: (needs a custom approach or a different background style)
-      */}
-      {/* <div className="sm:px-4">
-        <div
-          className="w-full h-[9px] p-2"
-          style={{
-            background:
-              'linear-gradient(90deg, #fcfcfc 2px, transparent 1%) 50%, ' +
-              'linear-gradient(#fcfcfc 2px, transparent 1%) 50%, #1c1c1c',
-            backgroundPosition: '0 0',
-            backgroundSize: '3px 3px',
-          }}
-        />
-      </div>*/}
       <div className="">
         <HR />
       </div>
 
       <footer
-        className="container max-w-2xl
-      mx-auto px-6 pb-12 sm:pb-8"
+        className={`container max-w-2xl mx-auto px-6 ${
+          isBlogPage ? 'sm:pb-12' : 'sm:pb-4'
+        }`}
       >
         <div className="">
-          {/*
-            2x2 grid:
-            Row 1: [Patrick Prunty] | [Projects]
-            Row 2: [Stay Connected] | [Newsletter]
-          */}
           <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-start">
             {/* 3) Stay Connected (Row 2, Col 1) */}
             <div>
@@ -74,54 +57,6 @@ export default function Footer() {
                 )}
               </div>
             </div>
-
-            {/* 2) Projects (Row 1, Col 2) */}
-            {/*      <div>
-              <h2 className="text-md font-bold text-gray-800 dark:text-white mb-2">
-                Projects
-              </h2>
-              <p className="text-gray-600 text-md dark:text-[#999999] mt-2 mb-2">
-                Check out some of my projects by clicking below.
-              </p>
-              <div className="flex gap-3">
-                <Link href="/projects/icon1" className="block" target="_blank">
-                  <Image
-                    src="/icon.webp"
-                    alt="Project 1"
-                    className="rounded-md"
-                    width={48}
-                    height={48}
-                  />
-                </Link>
-                <Link href="/projects/icon2" className="block" target="_blank">
-                  <Image
-                    src="/icon.webp"
-                    alt="Project 2"
-                    className="rounded-md"
-                    width={48}
-                    height={48}
-                  />
-                </Link>
-                <Link href="/projects/icon3" className="block" target="_blank">
-                  <Image
-                    src="/icon.webp"
-                    alt="Project 3"
-                    className="rounded-md"
-                    width={48}
-                    height={48}
-                  />
-                </Link>
-                <Link href="/projects/icon4" className="block" target="_blank">
-                  <Image
-                    src="/icon.webp"
-                    alt="Project 4"
-                    className="rounded-md"
-                    width={48}
-                    height={48}
-                  />
-                </Link>
-              </div>
-            </div>*/}
 
             {/* 4) Newsletter (Row 2, Col 2) */}
             <div>
