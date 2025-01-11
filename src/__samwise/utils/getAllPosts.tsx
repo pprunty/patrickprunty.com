c7a54a6983d59646fbfc488a048acd8c35a0b679
pprunty <pprunty@tcd.ie>
2025-01-11 12:43:13 +0000

    test

--- src/__samwise/utils/getAllPosts.tsx
+++ src/__samwise/utils/getAllPosts.tsx
@@ -18,31 +18,23 @@ export type Post = {
 };
 
 export const getAllPosts = async (filterDrafts: boolean = false): Promise<Post[]> => {
-  console.time('getAllPosts Execution');
-
   // Fetch views and posts in parallel
   const [allViews, posts] = await Promise.all([
     redis.hgetall('views'),
     Promise.resolve(postsData.posts),
   ]);
 
-  console.time('Map Posts with Views');
   const processedPosts = posts.map((post) => ({
     ...post,
     views: Number(allViews?.[post.slug] ?? 0),
     // Defer formatting for better performance
   }));
-  console.timeEnd('Map Posts with Views');
 
   // Filter drafts if required
   if (filterDrafts) {
-    console.time('Filter Draft Posts');
     const filteredPosts = processedPosts.filter((post) => !post.draft);
-    console.timeEnd('Filter Draft Posts');
-    console.timeEnd('getAllPosts Execution');
     return filteredPosts;
   }
 
-  console.timeEnd('getAllPosts Execution');
   return processedPosts;
 };
