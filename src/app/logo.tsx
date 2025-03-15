ad77ed837931f5e32aa4205436063ca1af53b747
pprunty <pprunty@tcd.ie>
2025-03-15 21:10:01 +0000

    remove logo animation

--- src/app/logo.tsx
+++ src/app/logo.tsx
@@ -2,70 +2,16 @@
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import { AUTHOR } from '@/config';
-import { useEffect, useRef, useState } from 'react';
+import { useRef } from 'react';
 
 export function Logo() {
   const pathname = usePathname();
-  const [shouldAnimate, setShouldAnimate] = useState(false);
-  const logoRef = useRef<HTMLSpanElement | null>(null); // Reference for the logo element
-  const [isInView, setIsInView] = useState(false); // Track if the component is in view
-
-  // Configure whether to include subpaths ("/blog/*")
-  const includeSubPaths = true; // Change to false to trigger only on "/blog"
-
-  // Intersection Observer to track visibility
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        setIsInView(entry.isIntersecting); // Update state when in/out of view
-      },
-      { threshold: 0.1 }, // Trigger when 10% of the component is in view
-    );
-
-    if (logoRef.current) {
-      observer.observe(logoRef.current);
-    }
-
-    return () => {
-      if (logoRef.current) observer.unobserve(logoRef.current);
-    };
-  }, []);
-
-  // Trigger animations only when component is in view and matches route
-  useEffect(() => {
-    const shouldTriggerAnimation = includeSubPaths
-      ? pathname.startsWith('/blog') // Match "/blog" and all subpaths
-      : pathname === '/blog'; // Match only "/blog"
-
-    if (shouldTriggerAnimation && isInView) {
-      // Trigger the first animation after 0.8 seconds
-      const initialTimer = setTimeout(() => {
-        setShouldAnimate(true); // Start animation
-        setTimeout(() => setShouldAnimate(false), 1400); // Reset after the animation duration (1.2 seconds)
-      }, 900);
-
-      // Set up recurring animation every 4.3 seconds
-      const interval = setInterval(() => {
-        setShouldAnimate(true); // Start animation
-        setTimeout(() => setShouldAnimate(false), 1400); // Reset after the animation duration (1.2 seconds)
-      }, 4300); // Interval timing: 1.2 second animation + 3.1 seconds rest
-
-      // Cleanup both the initial timer and interval
-      return () => {
-        clearTimeout(initialTimer);
-        clearInterval(interval);
-      };
-    } else {
-      setShouldAnimate(false); // Stop animation if not on `/blog` or not in view
-    }
-  }, [pathname, isInView]);
-
-  const animationClass = shouldAnimate ? 'animate-custom-pulse' : '';
+  const logoRef = useRef(null);
 
   return (
     <span
-      ref={logoRef} // Attach the ref to track visibility
-      className={`text-md md:text-lg whitespace-nowrap font-bold transition-colors ${animationClass}`}
+      ref={logoRef}
+      className="text-md md:text-lg whitespace-nowrap font-bold transition-colors"
     >
       <Link
         href="/"
