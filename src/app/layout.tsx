911078e1f5bea09c0e271b4c9fb77ca3064cc540
pprunty <pprunty@tcd.ie>
2025-03-23 15:49:51 +0000

    fix padding mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -94,7 +94,7 @@ export default function RootLayout({
       </head>
       <body className="text-gray-800 dark:text-gray-300 pt-3">
         <Header />
-        <main className="p-6 z-[100] pt-5 sm:pt-6 min-h-screen max-w-2xl m-auto">
+        <main className="p-6 z-[100] pt-3 sm:pt-6 min-h-screen max-w-2xl m-auto">
           {children}
         </main>
         <ClientComponents />
