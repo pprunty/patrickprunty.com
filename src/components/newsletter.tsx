c2842d746152e3c87fddbe1a667a59a94d8c7419
pprunty <pprunty@tcd.ie>
2025-04-07 23:51:36 +0100

    fix list and sitemap.tsx

--- src/components/newsletter.tsx
+++ src/components/newsletter.tsx
@@ -11,7 +11,8 @@ const Newsletter: React.FC = () => {
       <div className="max-w-3xl mx-auto flex flex-col justify-center items-center text-center">
         {/* Description */}
         <p className="max-w-md mb-8 max-w-[320px] text-base">
-            Like what you're reading? Subscribe to get notified when I publish new posts.
+          Like what you&apos;re reading? Subscribe to get notified when I
+          publish new posts.
         </p>
         {/* Subscribe Form */}
         <Subscribe />
