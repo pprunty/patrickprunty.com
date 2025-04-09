23899e7180a19c48d00369946b04f86e110d2de6
pprunty <pprunty@tcd.ie>
2025-04-09 22:28:41 +0100

    update newsletter styles

--- src/app/blog/components/badge.tsx
+++ src/app/blog/components/badge.tsx
@@ -8,7 +8,7 @@ interface BadgeProps {
 export function Badge({ className = '' }: BadgeProps) {
   return (
     <span
-      className={`inline-flex items-center gap-1 rounded-md px-2 py-1 text-[9px] sm:text-[11px] font-medium my-1 font-mono bg-green-100 text-green-800 ml-2 ${className}`}
+      className={`inline-flex items-center gap-1 rounded-md px-2 py-1 text-[9px] sm:text-[11px] font-medium my-1 font-mono bg-green-100 text-green-800 ml-1 ${className}`}
     >
       <Check size={12} weight="bold" />
       READ
