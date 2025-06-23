776ddf0ecb68f0df5733bac6f5fbf81c2c770046
pprunty <pprunty@tcd.ie>
2025-06-23 20:12:37 +0100

    updates

--- src/components/substack-button.tsx
+++ src/components/substack-button.tsx
@@ -29,7 +29,7 @@ const SubstackButton: React.FC<SubstackButtonProps> = ({
     return (
       <Button
         asChild
-        className={`bg-primary hover:bg-[#2d2c2c] dark:hover:bg-[#e0e0e0] active:bg-[#1a1a1a] dark:active:bg-[#d0d0d0] dark:text-black text-white border-0 ${className}`}
+        className={`bg-primary hover:bg-[#2d2c2c] dark:hover:bg-[#DEE8DE] active:bg-[#1a1a1a] dark:active:bg-[#d0d0d0] dark:text-black text-white border-0 ${className}`}
         {...props}
       >
         <a
